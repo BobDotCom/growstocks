a4c3744cad9ed01e00569c4b52fffece6a457824
BobDotCom <bobdotcomgt@gmail.com>
2021-04-14 00:43:53 -0500

    Bump version: 0.2.3 → 0.3.0

--- growstocks/__init__.py
+++ growstocks/__init__.py
@@ -46,7 +46,7 @@ import urllib3
 # PACKAGE INFO
 __title__ = "growstocks"
 __author__ = 'BobDotCom'
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021 {}".format(__author__)
