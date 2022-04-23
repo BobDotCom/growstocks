29cd6879db7322a41fea59d3be520dfac9a226e1
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-04-23 14:11:58 -0500

    Bump version: 1.0.3 → 1.1.0

--- growstocks/__init__.py
+++ growstocks/__init__.py
@@ -32,7 +32,7 @@ from .wrapper import *
 # PACKAGE INFO
 __title__ = "growstocks"
 __author__ = 'BobDotCom'
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021 {}".format(__author__)
